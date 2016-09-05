fd66ae240ed564fcd7dbd395f88f5bec2a9c5e62
frost <kharchilindanila@gmail.com>
2016-09-06 01:22:32 +0400

    q

--- index.py
+++ index.py
@@ -1,35 +1,70 @@
+# 
+
+
 class AbstractEvent:
 
 	def __init__(self, name, code):
 		self.name = name
 		self.code = code
 
+
 class Command(AbstractEvent):
 	pass
+
 
 class Event(AbstractEvent):
 	pass
 
-# ------------------------------------------
 
 class State:
 
 	def __init__(self, name):
 		self.name = name
-		actions = []
-		transitions = {}
+		self.actions = []
+		self.transitions = {}
 
-	def add_transition(event, target_state):
-		assert null != target_state
-		transitions[event.code] = Transition(self, event, target_state)
+	def add_transition(self, event, target_state):
+		assert target_state
+		self.transitions[event.code] = Transition(self, event, target_state)
+
+	def get_all_targets(self):
+		result = []
+		for key in self.transitions:
+			result.append(self.transitions[key].target)
+		return result
 
 
 class Transition:
 
-	def __init__(source, trigger, target):
+	def __init__(self, source, trigger, target):
 		self.source = source
 		self.trigger = trigger
 		self.target = target
+
+	def get_event_code(self):
+		return self.trigger.code
+
+
+class StateMachine:
+
+	def __init__(self, start):
+		self.start = start
+
+	def get_states(self):
+		result = []
+		self.collect_states(result, self.start)
+		return result
+
+	def collect_states(self, result, start):
+		if start in result:
+			return
+		result.append(start)
+		for next_ in start.get_all_targets():
+			self.collect_states(result, next_)
+
+	def add_reset_events(self, events):
+		for e in events:
+			self.reset_events.append(e)
 
 
 if __name__ == '__main__':
---
+++ test.py
@@ -0,0 +1,64 @@
+import unittest
+from index import (
+	Command,
+	Event,
+	State,
+	Transition,
+	StateMachine,
+	State,
+
+)
+
+
+class TestCase(unittest.TestCase):
+
+	def test_command_and_event(self):
+		c = Command('open_door_1', 'OPD1')
+		e = Event('door_1_open', 'D1OP')
+		
+		self.assertEquals(c.name, 'open_door_1')
+		self.assertEquals(c.code, 'OPD1')
+
+	def test_state(self):
+		state = State('door_1_closed')
+		target_state = State('door_1_opened')
+		event = Event('door_1_open', 'D1OP')
+		state.add_transition(event, target_state)
+		
+		self.assertIn(event.code, state.transitions)
+		transition = state.transitions[event.code]
+		self.assertIsInstance(transition, Transition)
+
+	def test_get_all_targets(self):
+		d1cl = State('door_1_closed')
+		walg = State('wait_light')
+		lgon = State('light_on')
+		
+		door_1_open = Event('door_1_open', 'D1OP')
+		light_on = Event('light_on', 'LGON')
+		
+		d1cl.add_transition(door_1_open, walg)
+		walg.add_transition(light_on, lgon)
+
+		self.assertEquals([walg], d1cl.get_all_targets())
+
+	def test_transition(self):
+		source = State('s1')
+		trigger = Event('s1_to_s2', 's12')
+		target = State('s2')
+		t = Transition(source, trigger, target)
+
+		self.assertEquals(source, t.source)
+		self.assertEquals(trigger, t.trigger)
+		self.assertEquals(target, t.target)
+
+	def test_machine(self):
+		start = State('s1')
+		sm = StateMachine(start)
+		states = sm.get_states()
+		
+		self.assertEquals([], states)
+
+
+if __name__ == '__main__':
+	unittest.main()
